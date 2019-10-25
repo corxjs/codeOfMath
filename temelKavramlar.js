53a2ca602b54b587d8dd56ea34300a83c2aa8c68
Mustafa Çor <mmustafaa9@gmail.com>
2019-10-25 22:23:19 +0300

    Update temelKavramlar.js

--- temelKavramlar.js
+++ temelKavramlar.js
@@ -5,13 +5,13 @@ class GercekSayi {
         this.basamak = this.basamakSayisiBul();
         this.rakamMi = this.rakamMiBul()
     }
-    rakamMiBul(){
-        return  this.deger < 10 && this.deger >=0;
+    rakamMiBul() {
+        return this.deger < 10 && this.deger >= 0;
     }
     basamakSayisiBul() {
         let temp = this.deger;
         let sayac = 0;
-        if(temp < 0){temp = temp * -1}
+        if (temp < 0) { temp = temp * -1 }
         while (temp !== 0) {
             temp = Math.floor(temp / 10);
             ++sayac;
@@ -19,29 +19,42 @@ class GercekSayi {
         return sayac;
     }
 }
-class RasyonelSayi extends GercekSayi{
-    constructor(sayi){
+class RasyonelSayi extends GercekSayi {
+    constructor(sayi) {
         super(sayi)
-        if(sayi == Infinity || typeof sayi == NaN){
+        if (sayi == Infinity || typeof sayi == NaN) {
             console.error("Rasyonel Sayı Olamaz.");
         }
-        
+
     }
 }
-class TamSayi extends RasyonelSayi{
-    constructor(sayi){
+class TamSayi extends RasyonelSayi {
+    constructor(sayi) {
         super(sayi);
         this.sayi = sayi;
-        if(!Number.isInteger(sayi)){
-           console.error("Tam Sayı Olamaz.");
+        if (!Number.isInteger(sayi)) {
+            console.error("Tam Sayı Olamaz.");
         }
     }
-    Faktoriyel(x){
-        if(x == null) x = this.sayi;
-        if(x == 1 ||x == 0) return 1;
-        return x * this.Faktoriyel(x-1);
+
+}
+class DogalSayi extends TamSayi {
+    constructor(sayi) {
+        super(sayi);
+        if (sayi < 0 || !Number.isInteger(sayi)) {
+            console.error("Doğal Sayı Olamaz.");
+        }
+        this.faktoriyel = this.Faktoriyel();
+    }
+
+    Faktoriyel(x) {
+        if (x == null) x = this.sayi;
+        if (x == 1 || x == 0) return 1;
+        return x * this.Faktoriyel(x - 1);
+      
     }
 }
 
-
+let x = new DogalSayi(7);
+console.log(x);
 
