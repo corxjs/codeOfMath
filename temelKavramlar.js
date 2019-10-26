d716e0af4c5ac2517d7eb5e37e32a2aead26ff85
Mustafa Çor <mmustafaa9@gmail.com>
2019-10-26 16:05:23 +0300

    translation

--- temelKavramlar.js
+++ temelKavramlar.js
@@ -1,86 +1,81 @@
-/* MATEMATİK */
-class GercekSayi {
-    constructor(sayi) {
-        this.deger = sayi;
-        this.basamak = this.basamakSayisiBul();
-        this.rakamMi = this.rakamMiBul()
+/* MATH 1*/
+class Real {
+    constructor(number) {
+        this.value = number;
+        this.digitNumber = this.DigitNumber();
+        this.isNumeral = this.IsNumeral()
     }
-    rakamMiBul() {
-        return this.deger < 10 && this.deger >= 0;
+    IsNumeral() {
+        return this.value < 10 && this.value >= 0;
     }
-    basamakSayisiBul() {
-        let temp = this.deger;
-        let sayac = 0;
+    DigitNumber() {
+        let temp = this.value;
+        let counter = 0;
         if (temp < 0) { temp = temp * -1 }
         while (temp !== 0) {
             temp = Math.floor(temp / 10);
-            ++sayac;
+            ++counter;
         }
-        return sayac;
+        return counter;
     }
 }
-class RasyonelSayi extends GercekSayi {
-    constructor(sayi) {
-        super(sayi)
-        if (sayi == Infinity || typeof sayi == NaN) {
-            console.error("Rasyonel Sayı Olamaz.");
+class Rational extends Real {
+    constructor(number) {
+        super(number)
+        if (number == Infinity || typeof number == NaN) {
+            console.error(`${number} Can't be rational`);
         }
-
     }
 }
-class TamSayi extends RasyonelSayi {
-    constructor(sayi) {
-       
-        super(Number.parseInt(sayi));
-        this.tekMi = sayi%2==0?false:true;
-        this.sayi = Number.parseInt(sayi);
-        if (!Number.isInteger(sayi)) {
-            console.error("Tam Sayı Olamaz.");
-        }
+class Integer extends Rational {
+    constructor(number) {
+        super(Number.parseInt(number));
+        this.isOdd = number%2==0?false:true;
+        this.number = Number.parseInt(number);
+        if (!Number.isInteger(number))console.error(`${number} Can't ınteger`);
     }
-
 }
-class DogalSayi extends TamSayi {
-    constructor(sayi) {
-        if (Number.parseInt(sayi)<0) {
-            console.error(`${sayi} Negatif Sayı Doğal Sayı Olamaz.`);
-            sayi = -1 * sayi;
+class Natural extends Integer {
+    constructor(number) {
+        if (Number.parseInt(number)<0) {
+            console.error(`${number} Negatif Sayı Doğal Sayı Olamaz.`);
+            number = -1 * number;
         }
-        if (sayi < 0 || !Number.isInteger(sayi))console.error("Doğal Sayı Olamaz.");
-        super(sayi); 
-        this.faktoriyel = this.Faktoriyel();
-        this.asalMi = this.AsalMi();
-        this.asalCarpanlar = this.AsalCarpanlar();
+        if (number < 0 || !Number.isInteger(number))console.error(`${number} Can't be natural number`);
+        super(number); 
+        this.factorial = this.Factorial();
+        this.isPrime = this.IsPrime();
+        this.primeMultipliers = this.PrimeMultipliers();
     }
-    Faktoriyel(x) {
-        if (x == null) x = this.sayi;
-        if (x == 1 || x == 0) return 1;
-        return x * this.Faktoriyel(x - 1);
+    Factorial(number) {
+        if (number == null) number = this.number;
+        if (number == 1 || number== 0) return 1;
+        return number * this.Factorial(number - 1);
     }
-    AsalMi(sayi){
-        if (sayi == null) sayi = this.sayi;
-        sayi = Number.parseInt(sayi);
-        let asal = true;
-        for (let i = 2; i <= Number.parseInt(sayi/2); i++) {
-            if (sayi%i == 0) {
-                asal = false;
+    IsPrime(number){
+        if (number == null) number = this.number;
+        number = Number.parseInt(number);
+        let prime = true;
+        for (let i = 2; i <= Number.parseInt(number/2); i++) {
+            if (number%i == 0) {
+                prime = false;
             }        
         }
-        return asal;
+        return prime;
     }
-    AsalCarpanlar(sayi){
-        if (sayi == null) sayi = this.sayi;
-        sayi = Number.parseInt(sayi);
-        if (this.AsalMi(sayi)) return [sayi];
-        let carpanlar = [];
-        for (let i = 2; i < sayi; i++) {
-            if (this.AsalMi(i)) {
-                if (sayi % i == 0) {
-                    carpanlar.push(i);
+    PrimeMultipliers(number){
+        if (number == null) number = this.number;
+        number = Number.parseInt(number);
+        if (this.IsPrime(number)) return [number];
+        let multipliers= [];
+        for (let i = 2; i < number; i++) {
+            if (this.IsPrime(i)) {
+                if (number % i == 0) {
+                    multipliers.push(i);
                 }
             }
         }
-        return carpanlar;
+        return multipliers;
     }
     
 }
@@ -88,7 +83,7 @@ class DogalSayi extends TamSayi {
 
 
 
-let x = new DogalSayi(11)
+let x = new Natural(11)
  console.log(x);
 
 
